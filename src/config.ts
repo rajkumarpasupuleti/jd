6a51d5a784eff1f63750d073148643f866115f27
Raj Kumar <rajkumarr7102002@gmail.com>
2026-04-21 12:56:26 +0530

    changed ngrok url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
-export const API_BASE_URL =
-  'https://73b4-2405-201-c000-a89e-407c-2f10-b447-f473.ngrok-free.app'
+export const API_BASE_URL ='https://1ccb-2405-201-c000-a89e-407c-2f10-b447-f473.ngrok-free.app'
